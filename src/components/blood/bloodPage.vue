a0e93726ebc1dbb04964804cd170e808f9fc675d
GroungChar-L <13654008330@163.com>
2017-02-09 11:55:02 +0800

    2017-02-09

---
+++ src/components/blood/bloodPage.vue
@@ -0,0 +1,120 @@
+<template>
+  <div>
+    <div class="content ">
+      <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container bcolorheader ">
+          <div class="col-xs-1 htop">
+            <button class="button button button-plain button-border button-circle" @click="backMainPage"><i class="glyphicon glyphicon-arrow-left"></i></button>
+          </div>
+          <div>
+            <v-header style="height: 60px"></v-header>
+          </div>
+          <div class="col-xs-3">
+            <button type="button" class="btn btn-warning btn-sm" @click="bloodinputpage(1)">输入血糖</button>
+          </div>
+          <div class="col-xs-3">
+            <button type="button" class="btn btn-warning  btn-sm" @click="bloodinputpage(2)">查看血糖</button>
+          </div>
+          <!--<div class="col-xs-3">
+                <button type="button" class="btn btn-warning btn-sm" @click="bloodinputpage(3)">上传照片</button>
+              </div>-->
+          <div class="col-xs-3">
+            <button type="button" class="btn btn-warning  btn-sm" @click="bloodinputpage(4)">留言/回复<span class="badge">42</span></button>
+          </div>
+        </div>
+      </nav>
+      <div style="height: 120px">
+      </div>
+      <div>
+        <div>
+          <div v-show="bloodinput">
+            <v-bloodInput></v-bloodInput>
+          </div>
+          <div v-show="leaveMessage">
+            <v-leaveMessage></v-leaveMessage>
+          </div>
+          <div v-show="bloodHistory">
+            <v-bloodHistory></v-bloodHistory>
+          </div>
+          <div v-show="uploadPic">
+            <v-uploadPic></v-uploadPic>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import bloodHistory from '../blood/bloodhistory/bloodHistory';
+  import bloodInput from '../blood/bloodinput/bloodInput';
+  import leaveMessage from '../blood/leavemessage/leaveMessage';
+  import uploadPic from '../blood/uploadPic/uploadPic';
+  import header from '../header/header';
+  export default {
+    data() {
+      return {
+        bloodinput: true,
+        leaveMessage: false,
+        bloodHistory: false,
+        uploadPic: false
+      };
+    },
+    components: {
+      'v-header': header,
+      'v-bloodInput': bloodInput,
+      'v-leaveMessage': leaveMessage,
+      'v-bloodHistory': bloodHistory,
+      'v-uploadPic': uploadPic
+    },
+    methods: {
+      bloodinputpage: function (n) {
+        if (n === 1) {
+          this.bloodinput = true;
+          this.leaveMessage = false;
+          this.bloodHistory = false;
+          this.uploadPic = false;
+        }
+        if (n === 2) {
+          this.bloodinput = false;
+          this.leaveMessage = true;
+          this.bloodHistory = false;
+          this.uploadPic = false;
+        }
+        if (n === 3) {
+          this.bloodinput = false;
+          this.leaveMessage = false;
+          this.bloodHistory = true;
+          this.uploadPic = false;
+        }
+        if (n === 4) {
+          this.bloodinput = false;
+          this.leaveMessage = false;
+          this.bloodHistory = false;
+          this.uploadPic = true;
+        }
+      },
+      backMainPage: function () {
+        this.$router.push('/MainPage');
+      }
+    }
+  };
+
+</script>
+<style>
+  .bcolor {
+    background-color: rgb(240, 173, 78)
+  }
+  
+  .hg {
+    margin-left: 10px;
+  }
+  
+  .bcolorheader {
+    background-color: rgb(34, 159, 253)
+  }
+  
+  .htop {
+    margin-top: 10px;
+  }
+
+</style>
---
+++ src/components/blood/bloodinput/bloodInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <form role="form">
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1">早餐前</label>
+      <input type="text" class="form-control" id="" placeholder="Enter text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1">早餐后</label>
+      <input type="text" class="form-control " id="" placeholder="text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1">午餐前</label>
+      <input type="text" class="form-control" id="" placeholder="Enter text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1">午餐后</label>
+      <input type="text" class="form-control " id="" placeholder="text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1">晚餐前</label>
+      <input type="text" class="form-control" id="" placeholder="Enter text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1">晚餐后</label>
+      <input type="text" class="form-control " id="" placeholder="text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1"> 睡&nbsp;&nbsp;&nbsp;前</label>
+      <input type="text" class="form-control" id="" placeholder="Enter text">
+    </div>
+    <div class="form-group col-xs-6">
+      <label for="" class="col-xs-offset-1"> 凌&nbsp;&nbsp;&nbsp; 晨</label>
+      <input type="text" class="form-control " id="" placeholder="text">
+    </div>
+    <div class="form-group col-xs-1">
+    </div>
+    <div class="form-group col-xs-10">
+      <div style="height: 40px">
+      </div>
+      <button type="button" class="btn btn-primary btn-lg btn-block">提交输入血糖值</button>
+    </div>
+  </form>
+</template>
+<style>
+
+
+</style>
+<script>
+  export default {};
+
+</script>
